88bb99fe82942fee291275ac1eb9093517eb490a
flomueller23 <flomueller@mac.com>
2025-11-14 15:29:44 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -120,6 +120,23 @@ if st.session_state.spiel_started and not st.session_state.spieler:
 # Spielstand
     st.header("Spielstand")
     daten = []
+# Anzeige
+    for sp in sorted(st.session_state.spieler, key=lambda x: -x["punkte"]):
+        zeile = {"Spieler": sp["name"], "Punkte": round(sp["punkte"],1)}
+        for i in range(len(st.session_state.runden) - 1, -1, -1):
+            runde = st.session_state.runden[i]
+            if i < len(sp["einsaetze"]):
+                bonus_symbol = "★" if bonus_empfaenger_pro_runde[i] and sp["name"] in bonus_empfaenger_pro_runde[i] else ""
+                vorzeichen = "+" if sp['gewinne'][i] > 0 else ""
+                zeile[runde["name"]] = (
+                    f"E: {int(sp['einsaetze'][i])} | "
+                    f"P: {sp['plaetze'][i]} | "
+                    f"{vorzeichen}{round(sp['gewinne'][i],1)}{bonus_symbol}"
+                )
+        daten.append(zeile)
+
+    df = pd.DataFrame(daten)
+    st.dataframe(df, use_container_width=True, hide_index=True)
     
 # RUNDENVERWALTUNG
 if st.session_state.spiel_started and st.session_state.spieler:
@@ -221,24 +238,6 @@ if st.session_state.spiel_started and st.session_state.spieler:
         # Punktestand für nächste Runde aktualisieren
         for sp in st.session_state.spieler:
             zwischenpunkte[sp["name"]] += sp["gewinne"][runde_idx]
-            
-    # Anzeige
-    for sp in sorted(st.session_state.spieler, key=lambda x: -x["punkte"]):
-        zeile = {"Spieler": sp["name"], "Punkte": round(sp["punkte"],1)}
-        for i in range(len(st.session_state.runden) - 1, -1, -1):
-            runde = st.session_state.runden[i]
-            if i < len(sp["einsaetze"]):
-                bonus_symbol = "★" if bonus_empfaenger_pro_runde[i] and sp["name"] in bonus_empfaenger_pro_runde[i] else ""
-                vorzeichen = "+" if sp['gewinne'][i] > 0 else ""
-                zeile[runde["name"]] = (
-                    f"E: {int(sp['einsaetze'][i])} | "
-                    f"P: {sp['plaetze'][i]} | "
-                    f"{vorzeichen}{round(sp['gewinne'][i],1)}{bonus_symbol}"
-                )
-        daten.append(zeile)
-
-    df = pd.DataFrame(daten)
-    st.dataframe(df, use_container_width=True, hide_index=True)
 
     # AUTOMATISCHES SPEICHERN
     if "spielname" in st.session_state:
